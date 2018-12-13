16ad6bff9d5520928cc47d428ccd9dcdbb6f2584
PC <PC@DESKTOP-9T2GR3R>
2018-12-13 15:28:47 +0800

    Add Product

---
+++ src/agileassignment/ProductInterface.java
@@ -0,0 +1,22 @@
+
+package agileassignment;
+
+
+public interface ProductInterface {
+    
+    public String getProductName();
+
+    public void setProductName(String ProductName);
+
+    public int getProductQuantity();
+
+    public void setProductQuantity(int ProductQuantity);
+
+    public double getProductAmount();
+
+    public void setProductAmount(double ProductAmount);
+    
+    public double getPromotionPrice();
+
+    public void setPromotionPrice(double PromotionPrice);
+}
